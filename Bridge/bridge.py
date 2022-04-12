5542b0e62c3c2726f7b592555392022291245ea6
Unknown <hanno@vegapunk>
2022-04-11 22:21:35 -0300

    Update bridge.py and main.py

---
+++ Bridge/bridge.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+
+class Implementor(ABC):
+    @abstractmethod
+    def operationImpA(self):
+        pass
+
+    @abstractmethod
+    def operationImpB(self):
+        pass
+
+class Abstraction(ABC):
+    def __init__(self, implementation: Implementor):
+        self.implementation = implementation
+
+    def operationA(self):
+        pass
+
+    def operationB(self):
+        pass
+
+class RefinedAbstractionA(Abstraction):
+    def __init__(self, implementation: Implementor):
+        self.implementation = implementation
+
+    def operationC(self):
+        pass
+
+class RefinedAbstractionB(Abstraction):
+    def __init__(self, implementation: Implementor):
+        self.implementation = implementation
+
+    def operationD(self):
+        pass
+
+class ConcreteImplementorA(Implementor):
+    def operationImpA(self):
+        pass
+
+    def operationImpB(self):
+        pass
+
+class ConcreteImplementorB(Implementor):
+    def operationImpA(self):
+        pass
+
+    def operationImpB(self):
+        pass
+
+if __name__ == "__main__":
+    imp = ConcreteImplementorA()
+    abst = Abstraction(imp)
+

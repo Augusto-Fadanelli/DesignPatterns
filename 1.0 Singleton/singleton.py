e26bcd48e6e22eb23880a27e6ae9fb7fffc1c7b5
Unknown <hanno@vegapunk>
2022-04-13 12:21:04 -0300

    Update singleton.py

---
+++ 1.0 Singleton/singleton.py
@@ -0,0 +1,32 @@
+class Singleton():
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        if Singleton.__instance == None:
+            Singleton("Default", "Default")
+        return Singleton.__instance
+
+    def __init__(self, value1: str, value2: str):
+        if Singleton.__instance != None:
+            print('Erro! Singleton só pode ser instanciado uma única vez.')
+        else:
+            self.value1 = value1
+            self.value2 = value2
+            Singleton.__instance = self
+
+    def printData(self):
+        print(f'value1: {self.value1}\nvalue2: {self.value2}')
+
+if __name__ == "__main__":
+    s1 = Singleton('Abacaxi', 'Melão')
+    print(s1)
+    s1.printData()
+
+    s2 = Singleton.getInstance()
+    print(s2)
+    s2.printData()
+
+    s3 = Singleton('Açaí', 'Uva')
+    print(s3)
+

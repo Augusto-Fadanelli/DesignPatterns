67dc88b354d6e6967f9d9447ccbe83cbd8dd43e6
Augusto Fadanelli de Sousa <augustofadanelli@protonmail.com>
2022-05-05 18:19:37 -0300

    Criado o Facade

--- 5.6 CardGame/facadeDP.py
+++ 5.6 CardGame/facadeDP.py
@@ -1,9 +1,145 @@
-from compositeDP import *
+from heavenlyBranch import *
+from humanBranch import *
+from supportBranch import *
+from potionBranch import *
+from spellBranch import *
+from trapBranch import *
+from landBranch import *
 
 class Tree:
     '''
     Docstring:
     Facade for Composite
     '''
-    def __init__(self):
-        pass
+    def createHeavenlyBranch(self):
+        magician = Magician()
+        sorcerer = Sorcerer()
+        elf = Elf()
+
+        heavenly_branch = Heavenly()
+
+        # Magician Leafs
+        magician.add(DarkMagician())
+        magician.add(LightMagician())
+
+        # Magician Branch
+        heavenly_branch.add(magician)
+
+        # Sorcerer Leafs
+        sorcerer.add(DarkSorcerer())
+        sorcerer.add(LightSorcerer())
+
+        # Sorcerer Branch
+        heavenly_branch.add(sorcerer)
+
+        # Elf Leafs
+        elf.add(ShapeshifterElf())
+        elf.add(SummonerElf())
+        elf.add(ElfArcher())
+
+        # Elf Branch
+        heavenly_branch.add(elf)
+
+        return heavenly_branch
+
+    def createHumanBranch(self):
+        knight = Knight()
+        assassin = Assassin()
+
+        human_branch = Human()
+
+        # Knight Leafs
+        knight.add(Templar())
+        knight.add(Paladin())
+
+        # Knight Branch
+        human_branch.add(knight)
+
+        # Assassin Leafs
+        assassin.add(Ninja())
+        assassin.add(StealthKiller())
+        assassin.add(Fighter())
+
+        # Assassin Branch
+        human_branch.add(assassin)
+
+        return human_branch
+
+    def createSupportBranch(self):
+        divine = Divine()
+        druid = Druid()
+
+        support_branch = Support()
+
+        # Divine Leafs
+        divine.add(Cleric())
+
+        # Divine Branch
+        support_branch.add(divine)
+
+        # Druid Leafs
+        druid.add(Nymph())
+
+        # Druid Branch
+        support_branch.add(druid)
+
+        return support_branch
+
+    def createPotionBranch(self):
+        potion_branch = Potion()
+
+        # Potion Leafs
+        potion_branch.add(CurePotion())
+        potion_branch.add(WeaknessPotion())
+        potion_branch.add(StrengthPotion())
+        potion_branch.add(PoisonPotion())
+
+        return potion_branch
+
+    def createSpellBranch(self):
+        healing_spell = HealingSpell()
+
+        spell_branch = Spell()
+
+        # Spell Leafs
+        spell_branch.add(FireRain())
+        spell_branch.add(TrailOfDead())
+
+        # Healing Spell Leafs
+        healing_spell.add(Esculapio())
+        healing_spell.add(NightHerbs())
+
+        # Healing Spell Branch
+        spell_branch.add(healing_spell)
+
+        return spell_branch
+
+    def createTrapBranch(self):
+        terrestrial = Terrestrial()
+
+        trap_branch = Trap()
+
+        # Trap Leafs
+        trap_branch.add(MeteorRain())
+        trap_branch.add(Necromancy())
+        trap_branch.add(AlternateReality())
+        trap_branch.add(Madness())
+
+        # Terrestrial Leafs
+        terrestrial.add(Thorns())
+        terrestrial.add(LightStatue())
+
+        # Terrestrial Branch
+        trap_branch.add(terrestrial)
+
+        return trap_branch
+
+    def createLandBranch(self):
+        land_branch = Land()
+
+        # Land Leafs
+        land_branch.add(FlatLand())
+        land_branch.add(Hell())
+        land_branch.add(Forest())
+
+        return land_branch
--- 5.6 CardGame/main.py
+++ 5.6 CardGame/main.py
@@ -1,12 +1,5 @@
 from facadeDP import *
 from compositeDP import *
-from heavenlyBranch import *
-from humanBranch import *
-from supportBranch import *
-from potionBranch import *
-from spellBranch import *
-from trapBranch import *
-from landBranch import *
 
 def showBranch(component: Component):
     print(f'RESULT: {component.operation()}')
@@ -17,190 +10,56 @@ def addLeaf(component1: Component, component2: Component):
 
     print(f'RESULT: {component1.operation()}')
 
-def createHeavenlyBranch(magician: Magician, sorcerer: Sorcerer, elf: Elf):
-    heavenly_branch = Heavenly()
-
-    # Magician Leafs
-    magician.add(DarkMagician())
-    magician.add(LightMagician())
-
-    # Magician Branch
-    heavenly_branch.add(magician)
-
-    # Sorcerer Leafs
-    sorcerer.add(DarkSorcerer())
-    sorcerer.add(LightSorcerer())
-
-    # Sorcerer Branch
-    heavenly_branch.add(sorcerer)
-
-    # Elf Leafs
-    elf.add(ShapeshifterElf())
-    elf.add(SummonerElf())
-    elf.add(ElfArcher())
-
-    # Elf Branch
-    heavenly_branch.add(elf)
-
-    return heavenly_branch
-
-def createHumanBranch(knight: Knight, assassin: Assassin):
-    human_branch = Human()
-
-    # Knight Leafs
-    knight.add(Templar())
-    knight.add(Paladin())
-
-    # Knight Branch
-    human_branch.add(knight)
-
-    # Assassin Leafs
-    assassin.add(Ninja())
-    assassin.add(StealthKiller())
-    assassin.add(Fighter())
-
-    # Assassin Branch
-    human_branch.add(assassin)
-
-    return human_branch
-
-def createSupportBranch(divine: Divine, druid: Druid):
-    support_branch = Support()
-
-    # Divine Leafs
-    divine.add(Cleric())
-
-    # Divine Branch
-    support_branch.add(divine)
-
-    # Druid Leafs
-    druid.add(Nymph())
-
-    # Druid Branch
-    support_branch.add(druid)
-
-    return support_branch
-
-def createPotionBranch():
-    potion_branch = Potion()
-
-    # Potion Leafs
-    potion_branch.add(CurePotion())
-    potion_branch.add(WeaknessPotion())
-    potion_branch.add(StrengthPotion())
-    potion_branch.add(PoisonPotion())
-
-    return potion_branch
-
-def createSpellBranch(healing_spell: HealingSpell):
-    spell_branch = Spell()
-
-    # Spell Leafs
-    spell_branch.add(FireRain())
-    spell_branch.add(TrailOfDead())
-
-    # Healing Spell Leafs
-    healing_spell.add(Esculapio())
-    healing_spell.add(NightHerbs())
-
-    # Healing Spell Branch
-    spell_branch.add(healing_spell)
-
-    return spell_branch
-
-def createTrapBranch(terrestrial: Terrestrial):
-    trap_branch = Trap()
-
-    # Trap Leafs
-    trap_branch.add(MeteorRain())
-    trap_branch.add(Necromancy())
-    trap_branch.add(AlternateReality())
-    trap_branch.add(Madness())
-
-    # Terrestrial Leafs
-    terrestrial.add(Thorns())
-    terrestrial.add(LightStatue())
-
-    # Terrestrial Branch
-    trap_branch.add(terrestrial)
-
-    return trap_branch
-
-def createLandBranch():
-    land_branch = Land()
-
-    # Land Leafs
-    land_branch.add(FlatLand())
-    land_branch.add(Hell())
-    land_branch.add(Forest())
-
-    return land_branch
-
 if __name__ == '__main__':
-    tree = Composite()
+    root = Composite()
+    tree = Tree()
 
     # Heavenly Branch
-    magician_branch = Magician()
-    sorcerer_branch = Sorcerer()
-    elf_branch = Elf()
-    heavenly_branch = createHeavenlyBranch(magician_branch, sorcerer_branch, elf_branch)
-
+    heavenly_branch = tree.createHeavenlyBranch()
     showBranch(heavenly_branch)
     print()
 
     # Human Branch
-    knight_branch = Knight()
-    assassin_branch = Assassin()
-    human_branch = createHumanBranch(knight_branch, assassin_branch)
-
+    human_branch = tree.createHumanBranch()
     showBranch(human_branch)
     print()
 
     # Support Branch
-    divine_branch = Divine()
-    druid_branch = Druid()
-    support_branch = createSupportBranch(divine_branch, druid_branch)
-
+    support_branch = tree.createSupportBranch()
     showBranch(support_branch)
     print()
 
     # Potion Branch
-    potion_branch = createPotionBranch()
-
+    potion_branch = tree.createPotionBranch()
     showBranch(potion_branch)
     print()
 
     # Spell Branch
-    healing_spell_branch = HealingSpell()
-    spell_branch = createSpellBranch(healing_spell_branch)
-
+    spell_branch = tree.createSpellBranch()
     showBranch(spell_branch)
     print()
 
     # Trap Branch
-    terrestrial_branch = Terrestrial()
-    trap_branch = createTrapBranch(terrestrial_branch)
-
+    trap_branch = tree.createTrapBranch()
     showBranch(trap_branch)
     print()
 
     # Land Branch
-    land_branch = createLandBranch()
-
+    land_branch = tree.createLandBranch()
     showBranch(land_branch)
     print()
 
     # Tree
-    tree.add(heavenly_branch)
-    tree.add(human_branch)
-    tree.add(support_branch)
-    tree.add(potion_branch)
-    tree.add(spell_branch)
-    tree.add(trap_branch)
-    tree.add(land_branch)
+    root.add(heavenly_branch)
+    root.add(human_branch)
+    root.add(support_branch)
+    root.add(potion_branch)
+    root.add(spell_branch)
+    root.add(trap_branch)
+    root.add(land_branch)
 
     print("Client: Now I've got a composite tree:")
-    showBranch(tree)
+    showBranch(root)
     print()
 
     #Adicionar Leaf direto na Tree

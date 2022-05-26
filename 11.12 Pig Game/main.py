770ee49e02cc8d98f7a52be69927d4c8fc389a9a
Augusto Fadanelli de Sousa <augustofadanelli@protonmail.com>
2022-05-26 09:41:42 -0300

    Criado o angry pig, ainda não foi implementado o flyweight

--- 11.12 Pig Game/main.py
+++ 11.12 Pig Game/main.py
@@ -3,22 +3,34 @@
 import pygame
 from pygame.locals import *
 from sys import exit
+import random
 
-# from PIL import Image
-#
-# def loadImage(image_name):
-#     fullname = os.path.join('Assets', image_name)
-#     try:
-#         image = pygame.image.load(fullname)
-#     except pygame.error:
-#         print(f'Cannot load image: {fullname}')
-#         raise SystemExit
-#     return image, image.get_rect()
-#
-#     # Teste
-#     #strip = Image.open('Assets/Pig Idle (36x30).png')
-#     #sprite = strip.crop(9 * 36, 0, 36, 30)
-#     # sprite = [strip.crop((9*36, 0, 36, 30)) for n in range (nb_of_sprites)]
+class angryPig(pygame.sprite.Sprite):
+    def __init__(self, posX, posY):
+        pygame.sprite.Sprite.__init__(self)
+        self.sprites = []
+        self.sprites.append(pygame.image.load('Assets/pig idle/Pig Idle 1.png'))
+        self.sprites.append(pygame.image.load('Assets/pig idle/Pig Idle 2.png'))
+        self.sprites.append(pygame.image.load('Assets/pig idle/Pig Idle 3.png'))
+        self.sprites.append(pygame.image.load('Assets/pig idle/Pig Idle 4.png'))
+        self.sprites.append(pygame.image.load('Assets/pig idle/Pig Idle 5.png'))
+        self.sprites.append(pygame.image.load('Assets/pig idle/Pig Idle 6.png'))
+        self.sprites.append(pygame.image.load('Assets/pig idle/Pig Idle 7.png'))
+        self.sprites.append(pygame.image.load('Assets/pig idle/Pig Idle 8.png'))
+        self.sprites.append(pygame.image.load('Assets/pig idle/Pig Idle 9.png'))
+        self.current = 0
+        self.image = self.sprites[self.current]
+        # self.image = pygame.transform.scale(self.image, (128*3, 64*3)) # Aumenta o tamanho da imagem
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = posX, posY
+
+    def update(self):
+        self.current += 0.3
+        if self.current >= len(self.sprites):
+            self.current = 0
+        self.image = self.sprites[int(self.current)]
+        # self.image = pygame.transform.scale(self.image, (128*3, 64*3)) # Aumenta o tamanho da imagem
 
 if __name__ == '__main__':
     pygame.init()
@@ -29,15 +41,24 @@ if __name__ == '__main__':
     pygame.display.set_caption('Angry Pig')
     clock = pygame.time.Clock()
 
+    all_sprites = pygame.sprite.Group()
+
     while True:
         clock.tick(30)
-        screen.fill((255,255,255))
+        screen.fill((220,230,230))
 
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 exit()
 
-            # Aqui ficam os controles
+            if event.type == KEYDOWN:
+                if event.key == K_q:
+                    posX = random.randint(1, screen_width)
+                    posY = random.randint(1, screen_height)
+                    pig = angryPig(posX, posY)
+                    all_sprites.add(pig)
 
-        pygame.display.update()
+        all_sprites.draw(screen)
+        all_sprites.update()
+        pygame.display.flip()
